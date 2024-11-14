d94b4d9ae10d6c2665281be37c45cea3d14ba11d
Daniil Futin <futinde@gmail.com>
2024-11-14 09:24:06 +0300

    Initialized the Calculator microservice.

---
+++ MS-Calculator/src/main/java/ru/neoflex/mscalculator/MsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.mscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCalculatorApplication.class, args);
+    }
+
+}
