3231223139c4f60d758d332ddbaf47e169491cf6
Daniil Futin <futinde@gmail.com>
2024-12-04 13:18:13 +0300

    Annotation changes and Swagger examples

--- MS-Calculator/src/main/java/ru/neoflex/mscalculator/dtos/EmploymentDto.java
+++ MS-Calculator/src/main/java/ru/neoflex/mscalculator/dtos/EmploymentDto.java
@@ -1,49 +1,48 @@
 package ru.neoflex.mscalculator.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import ru.neoflex.mscalculator.dtos.enumeration.EmploymentStatus;
 import ru.neoflex.mscalculator.dtos.enumeration.WorkPosition;
 
 import java.math.BigDecimal;
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Builder
 @Schema(description = "DTO, представляющее собой данные о занятости клиента")
 public class EmploymentDto {
 
     @NotNull
-    @Schema(description = "Тип занятости клиента")
+    @Schema(description = "Тип занятости клиента", example = "EMPLOYED")
     private EmploymentStatus employmentStatus;
 
     @NotNull
     @Size(min = 10, max = 12)
     @Pattern(regexp = "^\\d{10,12}$")
-    @Schema(description = "Идентификационный номер налогоплательщика работодателя клиента")
+    @Schema(description = "Идентификационный номер налогоплательщика работодателя клиента", example = "9921242264")
     private String employerINN;
 
     @NotNull
-    @Schema(description = "Ежемесячная зарплата клиента")
+    @Schema(description = "Ежемесячная зарплата клиента", example = "50000")
     private BigDecimal salary;
 
     @NotNull
-    @Schema(description = "Должность клиента")
+    @Schema(description = "Должность клиента", example = "JUNIOR")
     private WorkPosition position;
 
     @NotNull
     @Min(value = 0)
-    @Schema(description = "Суммарный опыт работы клиента в месяцах")
+    @Schema(description = "Суммарный опыт работы клиента в месяцах", example = "36")
     private Integer workExperienceTotal;
 
     @NotNull
     @Min(value = 0)
-    @Schema(description = "Опыт работы клиента на нынешнем месте работы в месяцах")
+    @Schema(description = "Опыт работы клиента на нынешнем месте работы в месяцах", example = "12")
     private Integer workExperienceCurrent;
+
+    @AssertTrue(message = "Work experience on a current job can exceed total work experience!")
+    public boolean isWorkExperienceCurrentValid() {
+        return workExperienceCurrent <= workExperienceTotal;
+    }
 }
--- MS-Calculator/src/main/java/ru/neoflex/mscalculator/dtos/LoanStatementRequestDto.java
+++ MS-Calculator/src/main/java/ru/neoflex/mscalculator/dtos/LoanStatementRequestDto.java
@@ -9,58 +9,55 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Schema(description = "DTO, содержащий детали кредитного запроса от клиента и его данные")
 @Builder
 public class LoanStatementRequestDto {
 
     @NotNull
     @Min(20000)
-    @Schema(description = "Сумма займа в рублях")
+    @Schema(description = "Сумма займа в рублях", example = "150000")
     private BigDecimal amount;
 
     @NotNull
     @Min(value = 6)
-    @Schema(description = "Срок кредита в месяцах")
+    @Schema(description = "Срок кредита в месяцах", example = "24")
     private Integer term;
 
     @NotNull
     @Size(min = 2, max = 30)
     @Pattern(regexp = "^[a-zA-Z]+$")
-    @Schema(description = "Имя клиента")
+    @Schema(description = "Имя клиента", example = "Джон")
     private String firstName;
 
     @NotNull
     @Size(min = 2, max = 30)
     @Pattern(regexp = "^[a-zA-Z]+$")
-    @Schema(description = "Фамилия клиента")
+    @Schema(description = "Фамилия клиента", example = "Доу")
     private String lastName;
 
     @Size(min = 2, max = 30)
     @Pattern(regexp = "^[a-zA-Z]+$")
-    @Schema(description = "Отчество клиента (при наличии)")
+    @Schema(description = "Отчество клиента (при наличии)", example = "Джонсович")
     private String middleName;
 
     @NotNull
     @Pattern(regexp = "^[a-z0-9A-Z_!#$%&'*+/=?`{|}~^.-]+@[a-z0-9A-Z.-]+$")
-    @Schema(description = "Адрес электронной почты клиента")
+    @Schema(description = "Адрес электронной почты клиента", example = "john.doe@mail.ru")
     private String email;
 
     @NotNull
-    @Schema(description = "Дата рождения клиента")
+    @Schema(description = "Дата рождения клиента", example = "1996-12-20")
     private LocalDate birthdate;
 
     @NotNull
     @Pattern(regexp = "\\d{4}")
-    @Schema(description = "Серия паспорта клиента")
+    @Schema(description = "Серия паспорта клиента", example = "3141")
     private String passportSeries;
 
     @NotNull
     @Pattern(regexp = "\\d{6}")
-    @Schema(description = "Номер паспорта клиента")
+    @Schema(description = "Номер паспорта клиента", example = "667430")
     private String passportNumber;
 
     @AssertTrue(message = "Applicant must be at least 18 years old")
